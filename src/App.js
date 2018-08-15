3a1b81dcbf740634a326f93acdb4abd5a869a77a
Silvija Prozinger <silvija.prozinger@gmail.com>
2018-08-15 18:59:25 +0200

    changed makeMarkerIcon from markerImage to Icon

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ class App extends Component {
 
     // Create default marker icon
     makeMarkerIcon(markerColor) {
-        const markerImage = new window.google.maps.MarkerImage(
+        const markerImage = new window.google.maps.Icon(
           'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
           '|40|_|%E2%80%A2',
           new window.google.maps.Size(21, 34),
